547d32fd9a8729f07a55ed8148d2f65fae86d577
shunmian <shunmian@gmail.com>
2017-10-15 19:20:10 +0800

    17. The Middleware Chain

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,39 +1,43 @@
 import { createStore } from 'redux'
 import todoApp from './reducers'
 
-const addLogSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  if(!console.group){
-    return rawDispatch;
+const loggerSupport = (store) => (next) => {
+  if (!console.group) {
+    return next;
   }
-  return (action)=>{
+  return (action) => {
     console.group(action.type)
     console.log('%c prev state', 'color: gray', store.getState())
-    console.log('%c action','color:blue',action)
-    const returnValue = rawDispatch(action)
+    console.log('%c action', 'color:blue', action)
+    const returnValue = next(action)
     console.log('%c next state', 'color:green', store.getState());
     console.groupEnd(action.type)
     return returnValue
   }
 }
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    if(typeof action.then === 'function'){
-      return action.then(rawDispatch)
-    }else{
-      return rawDispatch(action)
-    }
+const promiseSupport = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next)
+  } else {
+    return next(action)
   }
 }
 
-const configureStore = () =>{
-  const store =createStore(todoApp)
-  if(process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLogSupportToDispatch(store);
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware => {
+    store.dispatch = middleware(store)(store.dispatch)
+  })
+}
+
+const configureStore = () => {
+  const store = createStore(todoApp)
+  const middlewares = [promiseSupport]
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(loggerSupport);
   }
-  store.dispatch = addPromiseSupportToDispatch(store)
+  wrapDispatchWithMiddlewares(store, middlewares)
   return store
 }
 
